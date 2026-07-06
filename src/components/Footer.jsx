b0ffafaf8b25978dacf06141c4938a54bf22e1ae
Ara Z Baghdassarian <azbaghda@gmail.com>
2026-07-06 08:47:40 -0700

    git deploy test

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
       <div className="container">
         <div className="footer-inner">
           <div className="footer-left">
-            <p>© {new Date().getFullYear()} NATNA Community Foundation</p>
+            <p>© {new Date().getFullYear()} NATNA Community Foundation TEST</p>
           </div>
           <nav className="footer-nav">
             <Link to="/">Home</Link>
